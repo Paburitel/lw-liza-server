7cdf2ff681194ca9644ee666f51447e04380cc02
Nikolay Maligin <malyhinmikalai@gmail.com>
2019-05-11 15:48:24 +0300

    add users

--- src/schemas/index-schema.js
+++ src/schemas/index-schema.js
@@ -1,5 +1,5 @@
-import { UserSchema, UserModel } from "./user-schema";
+import { UserSchema, User } from "./user-schema";
 import { SmartphoneSchema, Smartphone } from "./smartphone-scema";
 import { Holder, HolderSchema } from "./holder-schema";
 
-export { UserSchema, UserModel, SmartphoneSchema, Smartphone, Holder, HolderSchema };
+export { UserSchema, User, SmartphoneSchema, Smartphone, Holder, HolderSchema };
