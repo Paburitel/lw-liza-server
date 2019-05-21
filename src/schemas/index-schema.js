a0c764bd827237f69fb0809c820069c8ad78f3c4
Nikolay Maligin <malyhinmikalai@gmail.com>
2019-05-21 18:58:26 +0300

    add order

--- src/schemas/index-schema.js
+++ src/schemas/index-schema.js
@@ -1,5 +1,6 @@
 import { UserSchema, User } from "./user-schema";
 import { SmartphoneSchema, Smartphone } from "./smartphone-scema";
 import { Holder, HolderSchema } from "./holder-schema";
+import  { OrderSchema, Order } from  "./order-schema"
 
-export { UserSchema, User, SmartphoneSchema, Smartphone, Holder, HolderSchema };
+export { UserSchema, User, SmartphoneSchema, Smartphone, Holder, HolderSchema, OrderSchema, Order };
